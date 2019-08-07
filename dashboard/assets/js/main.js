38369eb52fdc8a6eba3d67d8ca54b335d0d998c6
Krystian Wojcicki <kwojcicki@sympatico.ca>
2019-08-07 12:28:05 -0400

    Fixed spelling and removed unneeded label check

--- dashboard/assets/js/main.js
+++ dashboard/assets/js/main.js
@@ -49,9 +49,7 @@ function updateFunctionDescContent(jsonObject) {
     var replicas = jsonObject["replicas"];
     var dag = jsonObject["dag"];
     var description = "No flow description provided. Use faas-flow-desc in annotations";
-    if ("faas-flow-desc" in jsonObject["labels"]) {
-    description = jsonObject["labels"]["faas-flow-desc"];
-    } else if("faas-flow-desc" in jsonObject["annotations"]){
+    if("faas-flow-desc" in jsonObject["annotations"]){
     description = jsonObject["annotations"]["faas-flow-desc"]
     }
    
